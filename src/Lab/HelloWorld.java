439d7de84b4c18d182930488fcb4e8a1b8d1d318
Avi <praveenkumar@Praveens-MacBook-Pro.local>
2022-01-18 00:46:03 +0530

    first commit for Lab01 CS321

---
+++ src/Lab/HelloWorld.java
@@ -0,0 +1,19 @@
+package Lab;
+
+/**
+ * Name: Aditya Verma
+ * Date: January 16th, 2022
+ * Description: Lab 01 "HelloWorld Lab" Submission for CS321.
+ */
+public class HelloWorld {
+
+    /**
+     * Prints out "\nMy Hello World Program for Lab 01 for CS 321 at Bishop's University!\n"
+     * @param args A string array containing the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("***********************************************************************");
+        System.out.println("\nMy Hello World Program for Lab 01 for CS 321 at Bishop's University!\n");
+        System.out.println("***********************************************************************");
+    }
+}
